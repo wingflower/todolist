48cc7ca40598a31ca7d01cf1c86e8f7210b8b587
Dongho Gwak <wing.flower.k@gmail.com>
2023-12-22 00:05:05 +0900

    23_12_21 Todo 목록 날짜 형식 적용

--- lib/todo_item.dart
+++ lib/todo_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 import 'todo.dart';
 
@@ -26,7 +27,8 @@ class TodoItem extends StatelessWidget {
         style: TextStyle(color: todo.isDone ? Colors.grey : Colors.black),
       ),
       subtitle: Text(
-        '${todo.dateTime}',
+        DateFormat.yMMMd()
+            .format(DateTime.fromMicrosecondsSinceEpoch(todo.dateTime)),
         style: TextStyle(color: todo.isDone ? Colors.grey : Colors.black),
       ),
     );
