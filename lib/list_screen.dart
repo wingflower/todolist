a4249298ea934634b1f021c747f31e84678c2fdb
Dongho Gwak <wing.flower.k@gmail.com>
2023-12-21 23:00:16 +0900

    23_12_21 Todo 목록 &  Todo 작성 화면

---
+++ lib/list_screen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:todolist/create_screen.dart';
+
+class ListScreen extends StatelessWidget {
+  const ListScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Todo 리스티'),
+      ),
+      body: ListView(
+        children: const [
+          ListTile(
+            title: Text('title 1'),
+            subtitle: Text('subtitle 1'),
+          ),
+          ListTile(
+            title: Text('title 2'),
+            subtitle: Text('subtitle 2'),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => const CreateScreen()),
+          );
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todolist/list_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
                                                                                                                                 colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const ListScreen(),
     );
   }
 }
